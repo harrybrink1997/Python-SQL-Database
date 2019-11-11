02891e28f90d2214f0d7c958f7a2fbfba0f8f899
unknown <harry.brink@skyciv.com>
2019-11-12 09:56:47 +1100

    big changes

--- q3.py
+++ q3.py
@@ -1,4 +1,22 @@
 import psycopg2
+import sys
+
+
+courseName = sys.argv[1] if len(sys.argv) > 1 else "ENGG"
+# to be replaced for creating purposes
+courseName = "SOLA"
+
+query = '''select s.code, b.name
+from subjects s join courses c on (s.id = c.subject_id)
+join terms t on (c.term_id = t.id)
+join classes cl on (cl.course_id = c.id)
+join meetings m on (cl.id = m.class_id)
+join rooms r on (r.id = m.room_id)
+join buildings b on (b.id = r.within)
+where t.name like '19T2'
+and s.code like '{}%';'''.format(courseName)
+
+print("{}".format(view1))
 
 try:
     conn = psycopg2.connect("dbname='a3'")
@@ -8,38 +26,29 @@ except Exception as e:
 
 cur = conn.cursor()
 
-view1 = '''create or replace view getCourseEnrolments as
-select c.course_id, count(*)
-from course_enrolments c
-group by course_id;'''
-
-query = '''select s.code, c.quota, g.count
-from subjects s join courses c on (s.id = c.subject_id)
-join terms t on (c.term_id = t.id)
-join getCourseEnrolments g on (c.id = g.course_id)
-where t.name like '19T3'
-and c.quota > 50
-and g.count > c.quota
-order by s.code;'''
-
-try:
-    cur.execute(view1)
-except Exception as e:
-    print("Error creating view")
-    print (e)
-
 try:
     cur.execute(query)
 except Exception as e:
     print("Error selecting from table")
     print (e)
 
-rows = cur.fetchall()
+buildingDict = {}
 
-for tochange in rows:
+for code,name in cur.fetchall():
+
+    if name not in buildingDict:
+        buildingDict[name] = [code]
+    else:
+        courseCodes = buildingDict[name]
+        if code not in courseCodes:
+            courseCodes.append(code)
+
+for building in buildingDict:
+    print("{}".foramt(building))
+    courseCodes = buildingDict[building]
+    for i in range((len(courseCodes) - 1):
+        print(" {}".format(courseCodes[i]))
+
 
 
 conn.close()
-
-
-
