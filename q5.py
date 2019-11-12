a15086fae228b8e4bfed8e58034d34cbc085fe24
unknown <harry.brink@skyciv.com>
2019-11-12 16:47:41 +1100

    q5 d1

--- q5.py
+++ q5.py
@@ -3,18 +3,22 @@ import sys
 
 
 courseName = sys.argv[1] if len(sys.argv) > 1 else "COMP1521"
+courseName = "MATH5305"
 
-query = 
-
-select s.code
+query = '''select (100*g.count)::float/cl.quota as percentFull, cl.tag as class_tag, ct.name as class_type
 from subjects s join courses c on (s.id = c.subject_id)
 join terms t on (c.term_id = t.id)
-join getCourseEnrolments e on (c.id = e.course_id)
 join classes cl on (cl.course_id = c.id)
-join meetings m on (cl.id = m.class_id)
-where s.code like 'COMP1521'
-and t.name like '19T3';
-and g.count / 2 
+join getClassEnrolments g on (cl.id = g.class_id)
+join classtypes ct on (ct.id = cl.type_id)
+where s.code like '{}'
+and t.name like '19T3'
+order by ct.name, cl.tag, percentFull;'''.format(courseName)
+
+view1 = '''create or replace view getClassEnrolments as
+select c.class_id, count(*)
+from class_enrolments c
+group by class_id;'''
 
 
 try:
@@ -26,33 +30,33 @@ except Exception as e:
 cur = conn.cursor()
 
 try:
+    cur.execute(view1)
+except Exception as e:
+    print("Error creating view")
+    print (e)
+
+try:
     cur.execute(query)
 except Exception as e:
     print("Error selecting from table")
     print (e)
 
-termDict = {}
-for name,code,count in cur.fetchall():
+classDict = {}
+for percentFull, class_tag, class_type in cur.fetchall():
 
-    if name not in termDict:
-        termDict[name] = {}
-        codeCounts = termDict[name]
-        codeCounts[code] = count
+        if percentFull < 50:
 
-    else:
-        codeCounts = termDict[name]
-        if code not in codeCounts:
-            codeCounts[code] = count
+            classDict[class_tag] = {}
+
+            classInfo = classDict[class_tag]
+            classInfo["fullPercentage"] = round(percentFull)
+            classInfo["classtype"] = class_type
 
 
-for term in sorted(termDict):
-    print("{}".format(term))
-
-    codeCounts = termDict[term]
-
-    for course in sorted(codeCounts):
-       print(" {}({})".format(course, codeCounts[course]))
-
+for bookedClass in classDict:
+    classInfo = classDict[bookedClass]
+    print("{} {} is {}% full".format(classInfo["classtype"], bookedClass, classInfo["fullPercentage"]))
+    
 
 
 conn.close()
