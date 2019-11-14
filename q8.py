887dfc04d411dd9f9bb7db79d93a9762c29b097a
unknown <harry.brink@skyciv.com>
2019-11-15 10:27:55 +1100

    Checking q8 so far

--- q8.py
+++ q8.py
@@ -1,12 +1,43 @@
 import psycopg2
 import sys
 
+subjects = []  
+
 term = sys.argv[1] if len(sys.argv) > 1 else '19T1'
 
+term = '19T1'
+subjectsArray = ['COMP1511']
+
+classesQuery = []
+
+for subject in range(len(subjectsArray)):
+    classesQuery.append(
+        '''select s.code, ct.name, m.day, cl.tag, m.start_time, m.end_time
+        from courses c join terms t on (c.term_id = t.id)
+        join subjects s on (s.id = c.subject_id)
+        join classes cl on (cl.course_id = c.id)
+        join classtypes ct on (ct.id = cl.type_id)
+        join meetings m on (cl.id = m.class_id)
+        join rooms r on (r.id = m.room_id)
+        where t.name like '{}'
+        and r.code like 'K-%'
+        and s.code like '{}'
+        order by ct.name, m.day, m.start_time;'''.format(term,subjects[subject])
+    ) 
 
 
-query1 = 
 
+# select s.code, ct.name as classtype, m.day, cl.tag, m.start_time, m.end_time
+# from courses c join terms t on (c.term_id = t.id)
+# join subjects s on (s.id = c.subject_id)
+# join classes cl on (cl.course_id = c.id)
+# join classtypes ct on (ct.id = cl.type_id)
+# join meetings m on (cl.id = m.class_id)
+# join rooms r on (r.id = m.room_id)
+# where t.name like '19T1'
+# and r.code like 'K-%'
+# and s.code like 'MATH1081'
+# order by ct.name, m.day, m.start_time;
 
 try:
     conn = psycopg2.connect("dbname='a3'")
@@ -16,13 +47,53 @@ except Exception as e:
 
 cur = conn.cursor()
 
-try:
-    cur.execute(query1)
-except Exception as e:
-    print("Error selecting from table2")
-    print (e)
+# Gets all the classes for each course. 
+subjectClasses = {}
+for i in range(len(classesQuery)):
+    try:
+        cur.execute(classesQuery[i])
+    except Exception as e:
+        print("Error selecting from table2")
+        print (e)
 
-for replace in cur.fetchall():
+    subjectClasses[subjectsArray[i]] = {}
+    currSubject = subjectClasses[subjectsArray[i]]
+
+    for code, classtype, day, tag, start_time, end_time in cur.fetchall():
+        
+        if classtype not in currSubject:
+            currSubject[classtype] = {}
+            currClassType = currSubject[classtype]
+            currClassType[tag] = { 
+                "day": day, "start": start_time, "end": end_time
+            }
+        else:
+            currClassType = currSubject[classtype]
+            currClassType[tag] = { 
+                "day": day, "start": start_time, "end": end_time
+            }
+
+timeTables = {}
+for courses in subjectClasses:
+    addClasses(subjectClasses.get(courses)["Lectures"])
+
+
+
+def overLap(start1, end1, start2, end2):
+    return False
+
+def daysAtUni(days):
+    return 2
+
+def hoursInDay():
+    return 3
+
+def addClasses(classesDict):
+    print(classesDict)
+
+
+
+
 
 
 conn.close()
