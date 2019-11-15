efcb0c9e09b03e8c7164658dddb09ae59397fb12
unknown <harry.brink@skyciv.com>
2019-11-15 11:11:59 +1100

    Checking q8 so far error3

--- q8.py
+++ q8.py
@@ -1,100 +1,109 @@
 import psycopg2
 import sys
+import copy
 
-subjects = []  
+#Global variables
 
-term = sys.argv[1] if len(sys.argv) > 1 else '19T1'
+optimisedTT = []
+lowestTimeTableCost = None
 
-term = '19T1'
-subjectsArray = ['COMP1511']
+# Generate the queries for each course to call.
+def GetTimeTableQueries(subjectsArray):
 
-classesQuery = []
+    for subject in range(len(subjectsArray)):
 
-for subject in range(len(subjectsArray)):
-    classesQuery.append(
-        '''select s.code, ct.name, m.day, cl.tag, m.start_time, m.end_time
-        from courses c join terms t on (c.term_id = t.id)
-        join subjects s on (s.id = c.subject_id)
-        join classes cl on (cl.course_id = c.id)
-        join classtypes ct on (ct.id = cl.type_id)
-        join meetings m on (cl.id = m.class_id)
-        join rooms r on (r.id = m.room_id)
-        where t.name like '{}'
-        and r.code like 'K-%'
-        and s.code like '{}'
-        order by ct.name, m.day, m.start_time;'''.format(term,subjectsArray[subject])
-    ) 
+        classesQuery = []
+
+        classesQuery.append(
+            '''select s.code, ct.name, m.day, cl.tag, m.start_time, m.end_time
+            from courses c join terms t on (c.term_id = t.id)
+            join subjects s on (s.id = c.subject_id)
+            join classes cl on (cl.course_id = c.id)
+            join classtypes ct on (ct.id = cl.type_id)
+            join meetings m on (cl.id = m.class_id)
+            join rooms r on (r.id = m.room_id)
+            where t.name like '{}'
+            and r.code like 'K-%'
+            and s.code like '{}'
+            order by ct.name, m.day, m.start_time;'''.format(term,subjectsArray[subject])
+        ) 
+
+    return classesQuery
 
 
+def queryClassTT(classesQuery):
 
-# select s.code, ct.name as classtype, m.day, cl.tag, m.start_time, m.end_time
-# from courses c join terms t on (c.term_id = t.id)
-# join subjects s on (s.id = c.subject_id)
-# join classes cl on (cl.course_id = c.id)
-# join classtypes ct on (ct.id = cl.type_id)
-# join meetings m on (cl.id = m.class_id)
-# join rooms r on (r.id = m.room_id)
-# where t.name like '19T1'
-# and r.code like 'K-%'
-# and s.code like 'COMP1511'
-# order by ct.name, m.day, m.start_time;
-
-try:
-    conn = psycopg2.connect("dbname='a3'")
-except Exception as e:
-    print("Unable to connect to the database")
-    print(e)
-
-cur = conn.cursor()
-
-# Gets all the classes for each course. 
-subjectClasses = {}
-for i in range(len(classesQuery)):
     try:
-        cur.execute(classesQuery[i])
+        conn = psycopg2.connect("dbname='a3'")
     except Exception as e:
-        print("Error selecting from table2")
-        print (e)
+        print("Unable to connect to the database")
+        print(e)
 
-    subjectClasses[subjectsArray[i]] = {}
-    currSubject = subjectClasses[subjectsArray[i]]
-
-    for code, classtype, day, tag, start_time, end_time in cur.fetchall():
-        
-        if classtype not in currSubject:
-            currSubject[classtype] = []
-            currClassType = currSubject[classtype]
-            currClassType.append({ 
-                'tag': tag, 'day': day, 'start': start_time, 'end': end_time
-            })
-        elif classtype in currSubject:
-            currClassType = currSubject[classtype]
-            currClassType.append({ 
-                'tag': tag, 'day': day, 'start': start_time, 'end': end_time
-            })
-
-print(subjectClasses.get('COMP1511').get('Lecture'))
+    cur = conn.cursor()
 
 
+    subjectClasses = {}
+    for i in range(len(classesQuery)):
+        try:
+            cur.execute(classesQuery[i])
+        except Exception as e:
+            print("Error selecting from table2")
+            print (e)
 
-conn.close()
+        subjectClasses[subjectsArray[i]] = {}
+        currSubject = subjectClasses[subjectsArray[i]]
+
+        for code, classtype, day, tag, start_time, end_time in cur.fetchall():
+
+            if classtype not in currSubject:
+                currSubject[classtype] = []
+                currClassType = currSubject[classtype]
+                currClassType.append({ 
+                    'tag': tag, 'day': day, 'start': start_time, 'end': end_time
+                })
+            elif classtype in currSubject:
+                currClassType = currSubject[classtype]
+                currClassType.append({ 
+                    'tag': tag, 'day': day, 'start': start_time, 'end': end_time
+                })
+
+    print(subjectClasses.get('COMP1511'))
+
+    return subjectClasses
+
+    conn.close()
+
 
 def overLap(start1, end1, start2, end2):
-    return False
+    NotImplemented
 
 def daysAtUni(days):
-    return 2
+    NotImplemented
 
 def hoursInDay():
-    return 3
+    NotImplemented
 
+def totalHoursAndDays():
+    NotImplemented
 def addClasses(classesDict):
-    print(classesDict)
+    NotImplemented
+def addLectures(classDict):
+    NotImplemented
+def generateTermTT(courseClasses):
+    NotImplemented
 
-# timeTables = {}
-# for courses in subjectClasses:
-#     addClasses(subjectClasses.get(courses).get('Lectures'))
 
+if __name__ == "__main__":
+
+    if len(sys.argv > 1) and len(sys.argv <= 4):
+        subjectsArray = sys.argv[1:]
+    else:
+        subjectsArray = ['COMP1511', 'MATH1131']
+
+    queries = GetTimeTableQueries(subjectsArray)
+    courseClasses = queryClassTT(queries)
+    
+    # schedule = generateTermTT(courseClasses)
 
 
 
